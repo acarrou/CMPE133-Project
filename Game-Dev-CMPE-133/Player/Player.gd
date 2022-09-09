a01ae0ffcdf202ccfffe1004b27cb206a69d9031
Adrien Carrou <dridriswim@gmail.com>
2022-09-09 14:12:00 -0700

    adds different character sprite

--- Game-Dev-CMPE-133/Player/Player.gd
+++ Game-Dev-CMPE-133/Player/Player.gd
@@ -7,25 +7,30 @@ func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
-		$AnimatedSprite.flip_h = false
+		$AnimationSprite.flip_h = false
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
-		$AnimatedSprite.flip_h = true
+		$AnimationSprite.flip_h = true
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
+		
 
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	if (velocity.x or velocity.y != 0):
+		$AnimationSprite.animation = "Wizard Running"
+		velocity = move_and_slide(velocity)
+	else:
+		$AnimationSprite.animation = "Wizard Idle"
 
 func shoot():
 	add_child(load("res://Weapons&Spells/Bullet.tscn").instance())
 
 func spawn():
-	get_parent().add_child(load("res://Enemies/Enemy.tscn").instance())
+	get_parent().add_child(load("res://Enemies/Skeleton/Skeleton.tscn").instance())
 
 func enemyContact(enemyHitbox):
 	$DeathAnimationPlayer.play("Death")
